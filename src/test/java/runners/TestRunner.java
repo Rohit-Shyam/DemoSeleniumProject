c6909a246318b3972fae09d1a272a48ae4d2e267
Rohit Shyam <76948811+Rohit-Shyam@users.noreply.github.com>
2025-04-23 11:56:03 -0500

    Create TestRunner.java
    Added TestRunner.java

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,20 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",     // HTML report
+                "json:target/cucumber.json",              // JSON needed for Masterthought report
+                "junit:target/cucumber.xml"               // Optional: for CI tools like Jenkins
+        },
+        monochrome = true
+)
+public class TestRunner {
+}
