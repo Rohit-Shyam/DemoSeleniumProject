b0e2d8c95ec9d59c772bba236c20e405187730bc
Rohit Shyam <76948811+Rohit-Shyam@users.noreply.github.com>
2025-04-25 12:39:39 -0500

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,15 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"stepdefinitions"},
+        features = "src/test/resources/features",  // Path to feature files
+        glue = {"stepdefinitions"},  // Path to step definitions
         plugin = {
-                "pretty",
-                "html:target/cucumber-reports.html",     // HTML report
-                "json:target/cucumber.json",              // JSON needed for Masterthought report
-                "junit:target/cucumber.xml"               // Optional: for CI tools like Jenkins
+                "pretty",  // Readable format in the console
+                "html:target/cucumber-reports.html",  // HTML report
+                "json:target/cucumber.json",  // JSON needed for Masterthought report
+                "junit:target/cucumber.xml"  // Optional: For CI tools like Jenkins
         },
-        monochrome = true
+        monochrome = true,  // Output without escape characters
+        publish = true  // Automatically uploads to the Cucumber reports server (if required)
 )
 public class TestRunner {
 }
